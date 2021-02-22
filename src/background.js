d8ed6a85fd0e171949d0c769d354cb1327d3c4ab
Matthew Sweeney <msweeney@iwu.edu>
2021-02-21 18:52:48 -0600

    Have browser send credentials with each request.

--- src/background.js
+++ src/background.js
@@ -5,9 +5,16 @@
 
 // Send a HEAD request to Moodle
 function sendReq() {
-    req = new XMLHttpRequest();
-    req.open("HEAD", "https://courses.iwu.edu/", true);
-    req.send();
+    chrome.cookies.getAll({"domain": "https://courses.iwu.edu", "name": "IDMSESSID"}, (cookies) => {
+        req = new XMLHttpRequest();
+        req.open("HEAD", "https://courses.iwu.edu/", true);
+        // req.setRequestHeader("cookie", "IDMSESSID=test_value")
+        req.withCredentials = "true";
+        req.send();
+        console.log(req);
+        console.log("cookies found:");
+        console.log(cookies);
+    })
 }
 
 // Preserve the current cookie session by sending HEAD requests every so often
@@ -15,6 +22,7 @@ function preserve() {
     sendReq();
     // wait 5 mins worth of millisecs, then repeat
     setTimeout(preserve, 300000);
+    // setTimeout(preserve, 30000);  // for testing
 }
 
 preserve();
